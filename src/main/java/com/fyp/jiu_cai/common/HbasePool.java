b7c5bc49efbccf78e10632ae9ab15536d9d9d37e
fuyunpeng <fuyunpeng614@sina.com>
2019-04-12 17:42:36 +0800

    table 提交

--- src/main/java/com/fyp/jiu_cai/common/HbasePool.java
+++ src/main/java/com/fyp/jiu_cai/common/HbasePool.java
@@ -1,4 +1,13 @@
 package com.fyp.jiu_cai.common;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+
+import java.io.IOException;
+
+
 public class HbasePool {
+
 }
--- src/main/java/com/fyp/jiu_cai/controller/MainController.java
+++ src/main/java/com/fyp/jiu_cai/controller/MainController.java
@@ -1,4 +1,57 @@
 package com.fyp.jiu_cai.controller;
 
+import com.fyp.jiu_cai.common.HbasePool;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+
+import java.io.IOException;
+
 public class MainController {
+
+    public  String get (){
+        Configuration configuration =HBaseConfiguration.create();
+        configuration.set("hbase.master.info.bindAddress","47.95.15.154");
+
+        configuration.set("hbase.regionserver.info.bindAddres","47.95.15.154");
+
+        Connection connection=null;
+        Admin admin=null;
+        try {
+             connection =ConnectionFactory.createConnection(configuration);
+            admin = connection.getAdmin();
+            TableName tableName =TableName.valueOf("test");
+            Table table = connection.getTable(tableName);
+            Scan scan = new Scan();
+            scan.addColumn("".getBytes(),"".getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return  "";
+    }
 }
